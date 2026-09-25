47d5ce50536ad37dab3d7f33281b39e725c1ab17
Adharv Sunil Kaimal <u3275627@uni.canberra.edu.au>
2026-09-25 19:08:53 +1000

    Implement Patient Class IN stage04.py

--- stage04/stage04.py
+++ stage04/stage04.py
@@ -1,19 +1,46 @@
+from datetime import datetime # to handle dates in booking/appointment
+from enum import Enum # to handle status enums
+
+# implementation as per UML created in Stage03, worked from skeleton_structure.py
+# changes have been made to accomodate the new UML model
+
+# Handles enumeration for Appointment statuses
+class BookingStatus(Enum):
+    REQUESTED = "Requested"
+    CONFIRMED = "Confirmed"
+    CANCELLED = "Cancelled"
+
+# Handles enumeration for Appointment statuses
+class AppointmentStatus(Enum):
+    SCHEDULED = "Scheduled"
+    COMPLETED = "Completed"
+    CANCELLED = "Cancelled"
+
+# PATIENT Class
 class Patient:
     """Represents a patient in the SafeCare system."""
 
-    def __init__(self):
+    def __init__(self, patientID:str, name:str, phone:str, email:str):
         # Attributes: patientID, name, phone, email
-        pass
+        self.patientID = patientID
+        self.name = name
+        self.phone = phone
+        self.email = email
+        self.history = [] # appointment history IDs and booking logs
 
-    def update_contact_info(self):
-        # Update phone or email details
-        pass
+    def getHistory(self):
+        return self.history
 
-    def get_history(self):
-        # Return list of past bookings or appointments
-        pass
+    def updateContact(self, phone=None, email=None):
+        if phone:
+            self.phone = phone
+        if email:
+            self.email = email
+    
+    def getUniqueID(self):
+        return self.patientID
 
-
+# PRACTITIONER Class
 class Practitioner:
     """Represents a clinician member who sees patients."""
 
@@ -29,7 +56,7 @@ class Practitioner:
         # Add a new working time slot
         pass
 
-
+# APPOINTMENT Class
 class Appointment:
     """Represents a confirmed time slot on the calendar."""
 
